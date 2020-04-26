737f74bc5163e7258edaa009d593122cf6ce8ef6
Cole Hlava <cole.hlava@gmail.com>
2020-04-26 00:47:53 -0700

    Finished motion detection.

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,13 +1,14 @@
 # MotionDetector.py
 # Detects motion on a series of frames.
 
-import cv2
+import cv2, numpy
 
 class MotionDetector:
 
     def __init__(self, accumulatedWeight=0.5):
         self.accumulatedWeight = accumulatedWeight
         self.bg = None
+
 
     # Updates stored background
     def update(self, image):
@@ -19,7 +20,8 @@ class MotionDetector:
         # Update background
         cv2.accumulateWeighted(image, self.bg, self.accumulatedWeight)
 
-    # Detects motion
+
+    # Detects motion and determine coordinates around area of motion
     def detect(self, image, tolerance=25):
         # Compute the difference between current and previous frame
         delta = cv2.absdiff(self.bg.astype("uint8"), image)
@@ -31,10 +33,26 @@ class MotionDetector:
 
         # Find contours
         contours = cv2.findContours(threshold_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
+
+        # Version compatibility for cv2
+        if len(contours) == 2:
+            contours = contours[0]
+        else:
+            contours = contours[1]
+
         if len(contours) == 0:
             return None
 
+        (minX, minY) = (numpy.inf, numpy.inf)
+        (maxX, maxY) = (-numpy.inf, -numpy.inf)
+
         for c in contours:
-            # @TODO
+            # compute the bounding box of the contour and use it to
+            # update the minimum and maximum bounding box regions
+            (x, y, w, h) = cv2.boundingRect(c)
+            (minX, minY) = (min(minX, x), min(minY, y))
+            (maxX, maxY) = (max(maxX, x + w), max(maxY, y + h))
+
+        # Return a tuple of the coordinates of the motion bounding box
+        return (minX, minY, maxX, maxY)
 
--- web_interface.py
+++ web_interface.py
@@ -4,10 +4,114 @@
 import flask, cv2, datetime, threading, argparse
 from MotionDetector import MotionDetector
 
+# Global variables
+app = flask.Flask(__name__.split('.')[0])
+videoFeed = cv2.VideoCapture(0)
 outputFrame = None
 frameLock = threading.Lock()
 
-app = flask.Flask(__name__)
+@app.route("/")
+def index():
+    # Return the rendered html file
+    return flask.render_template("index.html")
 
-feed = cv2.VideoCapture(0)
+
+def detect_motion(frameCount):
+    global videoFeed, outputFrame, frameLock
+
+    md = MotionDetector()
+    total_frames = 0
+
+    while True:
+        # Capture frame
+        ret, frame = videoFeed.read()
+
+        # Format image into gray scale
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # grayscale image
+        gray_image = cv2.GaussianBlur(gray_image, (7, 7), 0)
+
+        # Timestamp the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
+            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+        if total_frames > frameCount:
+            # Detect motion
+            motion = md.detect(gray_image)
+            if motion is not None:
+                # unpack the tuple and draw the box surrounding the
+                # "motion area" on the output frame
+                (minX, minY, maxX, maxY) = motion
+                cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
+        else:
+            total_frames += 1
+        
+        # Update the background model
+        md.update(gray_image)
+
+        # Acquire the lock, update the output frame, then release the lock
+        with frameLock:
+            outputFrame = frame.copy()
+
+        # @NOTE: may cause issue if running in thread
+        # Cleanup and exit if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            videoFeed.release()
+            cv2.destroyAllWindows()
+            break
+
+
+# Generate a jpg file of the image and format into a byte array
+def generate():
+    # Grab global references to the output frame and frame lock variables
+    global outputFrame, frameLock
+
+    # Loop over frames from the output stream
+    while True:
+        # Wait until the lock is acquired
+        with lock:
+            if outputFrame is None:
+                continue
+
+            # Encode the frame into jpg format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+        # @NOTE make sure this is correct rv logic
+        # Ensure the frame was successfully encoded
+        if not flag:
+            continue
+
+        # Yield a byte array of the output frame
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+            bytearray(encodedImage) + b'\r\n')
+
+
+@app.route("/video_feed")
+def video_feed():
+    # Return the generated response and media type
+    return flask.Response(generate(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+# Begin program
+if __name__ == '__main__':
+    # Parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=True,
+        help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+        help="ephemeral port number of the server (1024 to 65535)")
+    ap.add_argument("-f", "--frame-count", type=int, default=32,
+        help="# of frames used to construct the background model")
+    args = vars(ap.parse_args())
+
+    # Start a thread that will perform motion detection
+    detection_thread = threading.Thread(target=detect_motion, args=(args["frame_count"],))
+    #t.daemon = True # NOTE try without
+    detection_thread.start()
+
+    # Start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True,
+        threaded=True, use_reloader=False)
 
